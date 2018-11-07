be62e4c672a48cf06e5a2a6e689f9dff50169ae7
github username <github email address>
2018-11-07 11:40:23 -0500

    That went about as well as expected. At least now I understand some of this better.

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
 
 class Filter extends Component {
+  // Ok, so something I've got to figure out, is why Filter is a class component - implying it does need to set its own state - whereas SongList and Song are functional components - implying they don't need a state.
+  // As of right now I feel the reason behind that is so it can set that to the songData state of its parent, which is how it'll alter KaraokeContainer's state.
+
+  // Nevertheless, I suppose the first thing to figure out is making sure the state props got passed down.
+
+  constructor(props) {
+    super(props)
+    console.log(props) // Ok... well for some reason songData has been passed down as a prop, but it's an empty array. Let's figure that out. I'm guessing that it's being rendered by KaraokeContainer before the state gets updated. That's likely got to do with the asynchronous nature of setState. How do I fix that? Am I approaching this the wrong way?
+    this.state = {
+      // songData: {this.props.songData} // Syntax error: this is a reserved word
+    }
+  }
+
+  // console.log(this.state)
+
   render() {
     return (
       <div className="filter">
--- src/containers/KaraokeContainer.js
+++ src/containers/KaraokeContainer.js
@@ -5,14 +5,52 @@ import KaraokeDisplay from '../components/KaraokeDisplay';
 import songs from '../data/songs';
 
 class KaraokeContainer extends Component {
+  // I am well aware that I will not be passing this code challenge; I don't have the understanding of React fundamentals necessary to do so. So, with this in mind, I am approaching this as more of a teachable exercise, making the most of these 90 minutes to try and wrap my mind around the few React concepts I do know, and making note of the many that I don't so that I am best prepared for next week's retake.
+
+  // Stands to reason that KaraokeContainer needs to be the one to fetch all the song data so it can pass all that information to its children.
+
+  // const API = 'http://localhost:4000/users/1/songs'
+  // Apparently I can't set the API to a const? - Syntax error: Unexpected token... WHY?
+
+  // OK, so, what do I have to do?
+    // I've got access to the song data from the API below. Nice. Now I've got to figure out how to pass that data down to all the children. How am I going to do that? Set a state I suppose.
+    // I don't have to use a constructor for it, since this is the top component, and therefore don't need to pass it props
+
+  // I am like 50% I've correctly set the state. How can I confirm this?
+  // How do I pass any of this down?
+
+  // Think, how should this app work?
+    // I need to access the song data, first and foremost. This will be stored in the state.
+    // Then, I need to be able to pass down that information to SongList and via SongList down to Song, where the songs will be displayed according to the state of its grandparent, KaraokeContainer.
+    // I then reckon it will be up to Filter to use an onChange event listener to alter the state of songData so that it is re-rendered in Song.js
+    // Where am I stuck? Passing down the state. I'm guessing I need to do this by setting props in Filter, SongList, and KaraokeDisplay. I'm going to try that now.
+
+  state = {
+    songData: [],
+    // test: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:4000/users/1/songs')
+      .then(resp => resp.json())
+      .then(parsedResp => {
+        const songData = parsedResp.map(song => {
+          return Object.assign({}, song)
+        })
+        this.setState({
+          songData: songData
+        }), console.log(this.state) // Cool, I've figured out how to check the state, I have indeed updated it.
+      })
+  }
+
   render() {
     return (
       <div className="karaoke-container">
         <div className="sidebar">
-          <Filter />
-          <SongList />
+          <Filter songData={this.state.songData} />
+          <SongList songData={this.state.songData} />
         </div>
-        <KaraokeDisplay />
+        <KaraokeDisplay songData={this.state.songData} />
       </div>
     );
   }
