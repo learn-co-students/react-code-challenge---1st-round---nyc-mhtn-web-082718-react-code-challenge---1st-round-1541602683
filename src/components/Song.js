309c1b9b9ca557042c48c707c85a9b659630fac7
JStern95 <JSternberg95@gmail.com>
2018-11-07 11:39:28 -0500

    Done enough

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-const Song = () => {
+const Song = (props) => {
+
   return (
     <tr className="song">
-      <td>title</td>
-      <td>singer</td>
-      <td><button>Play</button></td>
+      <td>{props.song.title}</td>
+      <td>{props.song.singer}</td>
+      <td>{props.song.likes}</td>
+      <td>{props.song.dislikes}</td>
+      <td>{props.song.plays}</td>
+      <td><button onClick={()=>props.showLyrics(props.song.id)}>Play</button></td>
     </tr>
   )
 }
