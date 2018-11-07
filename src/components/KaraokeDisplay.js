55b971e3ff17b3eab041477f30cc446ea3145dde
github username <github email address>
2018-11-07 11:49:00 -0500

    thanks

--- src/components/KaraokeDisplay.js
+++ src/components/KaraokeDisplay.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Lyrics from './Lyrics';
 
-const KaraokeDisplay = () => {
-  return (
-    <div className="karaoke-display">
-      <h2>Song Title</h2>
-      <Lyrics lyrics="example song lyrics" />
-    </div>
-  )
+const KaraokeDisplay = (props) => {
+
+  console.log(props.songs.songs)
+
+  const displaySong = props.songs.songs.filter( songObj => songObj.id === props.displayedSong )
+
+  console.log(displaySong)
+
+    return (
+      <div className="karaoke-display">
+        <h2>{displaySong.title}</h2>
+        <Lyrics lyrics="example song lyrics" />
+      </div>
+    )
+
 }
 
 export default KaraokeDisplay;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,12 +2,40 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 import KaraokeContainer from './KaraokeContainer';
 
+  const APIURL = 'http://localhost:4000/users/1/songs'
+
+
 class App extends Component {
+
+  state = {
+    songs: [],
+    displayedSong: 0
+  }
+
+  componentDidMount(){
+    fetch(APIURL)
+      .then(r => r.json())
+      .then(songs => {
+        this.setState({
+          songs: songs
+        })
+      })
+  }
+
+  handlePlayClick = (songId) => {
+    this.setState({
+      displayedSong: songId
+    })
+  }
+
   render() {
     return (
       <div className="app">
         <Header />
-        <KaraokeContainer />
+        <KaraokeContainer
+          songs={this.state.songs}
+          displayedSong={this.state.displayedSong}
+          handlePlayClick={this.handlePlayClick}/>
       </div>
     );
   }
