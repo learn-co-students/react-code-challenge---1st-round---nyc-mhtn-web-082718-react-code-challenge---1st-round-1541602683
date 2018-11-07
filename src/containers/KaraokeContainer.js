178a1c87ab4d1fac2656f099da4e2c83aca91bee
github username <github email address>
2018-11-07 11:43:15 -0500

    not good enough

--- src/containers/KaraokeContainer.js
+++ src/containers/KaraokeContainer.js
@@ -2,20 +2,50 @@ import React, { Component } from 'react';
 import Filter from '../components/Filter';
 import SongList from '../components/SongList';
 import KaraokeDisplay from '../components/KaraokeDisplay';
-import songs from '../data/songs';
-
+const theSongAPI = 'http://localhost:4000/users/1/songs'
 class KaraokeContainer extends Component {
+
+  state = {
+    songs: [],
+    currentSong: []
+  }
+
+  handleSongSelect(song) {
+    //Following steps:  Create a call back function to set current song to change when called
+    // in song onclick to tell karaokeDisplay what song to play. State.currentSong needs to be set.
+    // state.currentSong will be send down after it is set to karaokeDisplay
+    console.log(song)
+  }
+
+  getTheApi = () => {
+    fetch(theSongAPI)
+    .then((response) => {
+      return response.json();
+    })
+    .then((myJson) => {
+      this.setState({
+        songs: myJson
+      })
+    });
+  }
+
+  componentDidMount() {
+    this.getTheApi()
+  }
+
   render() {
     return (
       <div className="karaoke-container">
         <div className="sidebar">
           <Filter />
-          <SongList />
+          <SongList songs={this.state.songs} handleSongSelect={this.handleSongSelect()} />
         </div>
-        <KaraokeDisplay />
+        <KaraokeDisplay songs={this.state.songs} />
       </div>
     );
   }
 }
+
+
 
 export default KaraokeContainer;
